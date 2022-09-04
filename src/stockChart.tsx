f99ef985a6cbd29338143c055886fb4d43fcefb8
op1ekun <op1ekun@borntoco.de>
2022-09-04 23:43:37 +0200

    adds stock chart component leveraging highcharts

---
+++ src/stockChart.tsx
@@ -0,0 +1,80 @@
+import Highcharts from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
+import { 
+    IDataItemResult,
+    IDataQuery,
+    useData
+} from './useData';
+import { toSeries } from './utils/toSeries';
+
+export interface IStockChartProps {
+    query: IDataQuery
+}
+
+export const StockChart = (props: IStockChartProps) => {
+    const { query } = props;
+
+    const data = useData(query);
+
+    return (
+        <HighchartsReact
+            highcharts={Highcharts}
+            constructorType="stockChart"
+            options={{
+                title: {
+                    text: 'BH Stock Chart'
+                },
+                series: [
+                    {
+                        type: 'ohlc',
+                        name: 'OHLC',
+                        data: toSeries<IDataItemResult>(data, [ 
+                            'date',
+                            'open',
+                            'high',
+                            'low',
+                            'close'
+                        ])
+                    },
+                    {
+                        type: 'column',
+                        name: 'Volume',
+                        data: toSeries<IDataItemResult>(data, [
+                            'date',
+                            'volume'
+                        ]),
+                        yAxis: 1
+                    }
+                ],
+                xAxis: {
+                    title: {
+                        text: ''
+                    },
+                    type: 'datetime',
+                    crosshair: true
+                },
+                yAxis: [{
+                    title: {
+                        text: ''
+                    },
+                    height: '80%',
+                    resize: {
+                        enabled: true
+                    }
+                }, {
+                    title: {
+                        text: ''
+                    },
+                    labels: {
+                        enabled: false
+                    },
+                    name: 'Volume',
+                    top: '80%',
+                    height: '20%',
+                    offset: 0
+                }]
+            }}
+            
+        />
+    )
+};
